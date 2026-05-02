616699463d1d77a9a7498ff89893e11e639c939e
AmritNalam <45739058+AmritNalam@users.noreply.github.com>
2026-05-03 02:26:43 +0530

    Delete com/airtribe/learntrack directory

--- com/airtribe/learntrack/exception/EntityNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.airtribe.learntrack.exception;
-
-public class EntityNotFoundException extends Exception {
-    public EntityNotFoundException(String msg) { super(msg); }
-}
--- com/airtribe/learntrack/util/IdGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.airtribe.learntrack.util;
-
-public class IdGenerator {
-    private static int sid = 1, cid = 1, eid = 1;
-
-    public static int nextStudentId() { return sid++; }
-    public static int nextCourseId() { return cid++; }
-    public static int nextEnrollmentId() { return eid++; }
-}
